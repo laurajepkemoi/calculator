6a259f3bfacf9fc745ce739b1236fa862645a2e9
laurajepkemoi <39773589+laurajepkemoi@users.noreply.github.com>
2018-07-20 20:02:56 +0300

    Add files via upload

---
+++ function2.h
@@ -0,0 +1,18 @@
+#include 'functions.h'
+
+//a function implementation
+int add(int a,int b){
+		return a+b;
+	}
+int multiply(int a,int b){
+		return a*b;
+	}
+int subtract(int a,int b){
+		return a-b;
+	}
+int divide(int a,int b){
+		return a/b;
+	}
+int modulus (int a,int b){
+		return a%b;
+}
